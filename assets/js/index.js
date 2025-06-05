f0db1fd2b3f128f126bed38fd26e3f7666884098
hbk619 <hbk619@users.noreply.github.com>
2025-06-05 15:30:31 +0100

    feat: a blog!
    One post, theme, dark mode, WCAG AA compliant, passing IBM's checker apart from the skip to main content link not being in a landmark but there's no need for it to be in a landmark[1][2] as it's a single item and should be the first thing you navigate to on the page.
    
    [1]: https://dequeuniversity.com/rules/axe/4.0/region?application=RuleDescription
    [2]: https://www.drupal.org/project/drupal/issues/3098828#comment-13553038

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sysTheme = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark" : "light";
+
+    const localPreference = localStorage.getItem("theme");
+    const darkModeToggle = document.querySelector("#dark-mode-toggle");
+    const bodyElement = document.querySelector("body");
+
+    const isDark = localPreference ? localPreference === "dark" : sysTheme === "dark";
+    bodyElement.classList.toggle("dark", isDark);
+    darkModeToggle.textContent = `Enable ${isDark ? "light" : "dark"} mode`;
+
+    darkModeToggle.addEventListener("click", () => {
+        const isDark = bodyElement.classList.toggle("dark");
+        localStorage.setItem("theme", isDark ? "dark" : "light");
+        darkModeToggle.textContent = `Enable ${isDark ? "light" : "dark"} mode`;
+    });
+});
